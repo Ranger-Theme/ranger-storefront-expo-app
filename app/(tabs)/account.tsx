abe5ef8df685c5bac67d363a5eed60c309143b39
caesar <454451758@qq.com>
2024-09-20 15:28:41 +0800

    chroe: expo react native app

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,31 @@
+import { Image, StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
+
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+
+export default function AccountScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <Link href="/">Home</Link>
+      <Text>Account Page</Text>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
